016d452ba144e0033c37e7b80a4e128b6a921c45
Cesar <cesarvegac1854@gmail.com>
2025-08-20 13:31:37 -0600

    primer push
    INICIO

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void prueba() {
+    //Primera prueba
+    cout << "Primera prueba" << endl;
+}
+
+int main() {
+    prueba();
+    return 0;
+}
+
---
+++ src/main.h
@@ -0,0 +1,13 @@
+//
+// Created by cesar on 20/8/2025.
+//
+
+#ifndef PROYECTO1_DATOS2_MAIN_H
+#define PROYECTO1_DATOS2_MAIN_H
+
+
+class main {
+};
+
+
+#endif //PROYECTO1_DATOS2_MAIN_H
